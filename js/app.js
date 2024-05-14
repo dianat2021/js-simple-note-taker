f02c8daffcb1544fd4b2e0c1f73a3c9a73ecb72e
= <=>
2024-05-14 12:42:37 +0200

    fix class name

--- js/app.js
+++ js/app.js
@@ -1,1 +1,7 @@
-console.log("test");
+"user strict";
+
+const subjectInput = document.querySelector(".note-subject");
+const dateInput = document.querySelector(".note-date");
+const noteInput = document.querySelector(".note-input");
+
+const addNotes = () => {};
