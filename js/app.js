bb712030db0410e44fd81a04b1120f6dcc30775a
= <=>
2024-05-14 14:04:20 +0200

    set error message and test validation

--- js/app.js
+++ js/app.js
@@ -1,17 +1,32 @@
+import validateInputFields from "./inputValidation.js";
+
 const subjectInput = document.querySelector(".note-subject");
 const dateInput = document.querySelector(".note-date");
 const noteText = document.querySelector(".note-text");
+const error = document.querySelector(".error-message");
 const submitButton = document.querySelector(".submit-btn");
+
 const notesList = [];
 // ---------- FUNCTION THAT COLLECTS THE DATA AND ADDS IT TO THE LIST ON SUBMIT
 const addNotes = () => {
-  const currentNote = {
-    id: Date.now(),
-    subject: subjectInput.value,
-    date: dateInput.value,
-    text: noteText.value,
-  };
-  notesList.push(currentNote);
+  const isDataValid = validateInputFields(
+    subjectInput,
+    dateInput,
+    noteText,
+    error
+  );
+  if (!isDataValid) return;
+  else {
+    console.log(isDataValid);
+    const currentNote = {
+      id: Date.now(),
+      subject: subjectInput.value,
+      date: dateInput.value,
+      text: noteText.value,
+    };
+    notesList.push(currentNote);
+    console.log(notesList);
+  }
 };
 
 submitButton.addEventListener("click", addNotes);
