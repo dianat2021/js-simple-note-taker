34cbef9449c4ad2ad4bfad3201439c61d4be6ee4
= <=>
2024-05-14 13:04:20 +0200

    remove unnecessary element selections

--- js/app.js
+++ js/app.js
@@ -3,5 +3,3 @@
 const subjectInput = document.querySelector(".note-subject");
 const dateInput = document.querySelector(".note-date");
 const noteInput = document.querySelector(".note-input");
-
-const addNotes = () => {};
