48e44b376af51932af1a82ce95af6c0979ca4dd1
= <=>
2024-05-14 13:02:28 +0200

    add data to the array

--- js/app.js
+++ js/app.js
@@ -1,1 +1,17 @@
-console.log("test");
+const subjectInput = document.querySelector(".note-subject");
+const dateInput = document.querySelector(".note-date");
+const noteText = document.querySelector(".note-text");
+const submitButton = document.querySelector(".submit-btn");
+const notesList = [];
+// ---------- FUNCTION THAT COLLECTS THE DATA AND ADDS IT TO THE LIST ON SUBMIT
+const addNotes = () => {
+  const currentNote = {
+    id: Date.now(),
+    subject: subjectInput.value,
+    date: dateInput.value,
+    text: noteText.value,
+  };
+  notesList.push(currentNote);
+};
+
+submitButton.addEventListener("click", addNotes);
